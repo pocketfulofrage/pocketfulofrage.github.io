01253f6565fe70f03004f7b17990ed51de2ec16b
Russ <3870824+pocketfulofrage@users.noreply.github.com>
2021-05-20 10:23:33 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,38 +1,80 @@
-function onYouTubeIframeAPIReady() {
-  var player;
-  player = new YT.Player('YouTubeBackgroundVideoPlayer', {
-      videoId: '{$loadYouTubeVideoID}', // YouTube Video ID
-      width: 1280,               // Player width (in px)
-      height: 720,              // Player height (in px)
-      playerVars: {
-        playlist: '{$loadYouTubeVideoID}',
-          autoplay: 1,        // Auto-play the video on load
-          autohide: 1,
-          disablekb: 1, 
-          controls: 0,        // Hide pause/play buttons in player
-          showinfo: 0,        // Hide the video title
-          modestbranding: 1,  // Hide the Youtube Logo
-          loop: 1,            // Run the video in a loop
-          fs: 0,              // Hide the full screen button
-          autohide: 0,         // Hide video controls when playing
-          rel: 0,
-          enablejsapi: 1
-      },
-      events: {
-        onReady: function(e) {
-            e.target.mute();
-            e.target.setPlaybackQuality('hd1080');
-        },
-        onStateChange: function(e) {
-          if(e && e.data === 1){
-              var videoHolder = document.getElementById('home-banner-box');
-              if(videoHolder && videoHolder.id){
-                videoHolder.classList.remove('loading');
-              }
-          }else if(e && e.data === 0){
-            e.target.playVideo()
-          }
+var vid = "JKaWbVGewTg",
+    streams,
+    video_tag = document.getElementById("video");
+
+fetch("https://images"+~~(Math.random()*33)+"-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=https%3A%2F%2Fwww.youtube.com%2Fget_video_info%3Fvideo_id%3D" + vid).then(response => response.text()).then(function(data) {
+    if (data) {
+        streams = parse_youtube_meta(data);
+        video_tag.src = streams['1080p'] || streams['720p'] || streams['360p'];
+    } else {
+        alert('Youtube API Error');
+    }
+});
+
+function parse_youtube_meta(rawdata) {
+    var data = parse_str(rawdata),
+            player_response = JSON.parse(data.player_response),
+        streams = [],
+            result = {};
+    
+    data.player_response = player_response;
+    
+    if (data.hasOwnProperty('adaptive_fmts')) {
+            streams = streams.concat(data.adaptive_fmts.split(',').map(function(s) {
+                return parse_str(s)
+            }));
+        } else {
+            if (player_response.streamingData && player_response.streamingData.adaptiveFormats) {
+                streams = streams.concat(player_response.streamingData.adaptiveFormats);
+            }
         }
-      }
-  });
+        if (data.hasOwnProperty('url_encoded_fmt_stream_map')) {
+            streams = streams.concat(data.url_encoded_fmt_stream_map.split(',').map(function(s) {
+                return parse_str(s)
+            }));
+        } else {
+            if (player_response.streamingData && player_response.streamingData.formats) {
+                streams = streams.concat(player_response.streamingData.formats);
+            }
+        }
+    
+    streams.forEach(function(stream, n) {
+        var itag = stream.itag * 1,
+            quality = false,
+            itag_map = {
+                18: '360p',
+                22: '720p',
+                37: '1080p',
+                38: '3072p',
+                82: '360p3d',
+                83: '480p3d',
+                84: '720p3d',
+                85: '1080p3d',
+                133: '240pna',
+                134: '360pna',
+                135: '480pna',
+                136: '720pna',
+                137: '1080pna',
+                264: '1440pna',
+                298: '720p60',
+                299: '1080p60na',
+                160: '144pna',
+                139: "48kbps",
+                140: "128kbps",
+                141: "256kbps"
+            };
+        //if (stream.type.indexOf('o/mp4') > 0) console.log(stream);
+        if (itag_map[itag]) result[itag_map[itag]] = stream.url;
+    });
+    return result;
+};
+
+function parse_str(str) {
+    return str.split('&').reduce(function(params, param) {
+        var paramSplit = param.split('=').map(function(value) {
+            return decodeURIComponent(value.replace('+', ' '));
+        });
+        params[paramSplit[0]] = paramSplit[1];
+        return params;
+    }, {});
 }
